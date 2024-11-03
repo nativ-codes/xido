03229cafb3068ae0d2f2167dadd451b4df1e96ea
Andrei Otea <iAndrew3@protonmail.com>
2024-11-03 21:41:49 +0200

    Removed not used files + updated bottom tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,27 +1,62 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
+import Ionicons from '@expo/vector-icons/MaterialCommunityIcons';
 
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import colors from '@/common/colors';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: colors.secondaryText,
         headerShown: false,
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          // tabBarIcon: ({ color, focused }) => (
-          //   <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          // ),
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home' : 'home-outline'} size={24} color={color} />
+          ),
         }}
       />
+      <Tabs.Screen
+        name="companies"
+        options={{
+          title: 'Companies',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'database' : 'database-outline'} size={24} color={color} />
+
+          ),
+        }}
+      />      
+      <Tabs.Screen
+        name="calendar"
+        options={{
+          title: 'Calendar',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'card-text' : 'card-text-outline'} size={24} color={color} />
+          ),
+        }}
+      />        
+      <Tabs.Screen
+        name="goals"
+        options={{
+          title: 'Goals',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'star' : 'star-outline'} size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'cog' : 'cog-outline'} size={24} color={color} />
+          ),
+        }}
+      />                  
     </Tabs>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { DarkTheme, DefaultTheme } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -6,13 +6,10 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     Urbanist: require('@/assets/fonts/Urbanist-Regular.ttf'),
     UrbanistBold: require('@/assets/fonts/Urbanist-Bold.ttf'),
@@ -30,7 +27,6 @@ export default function RootLayout() {
 
   return (
     <SafeAreaProvider>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="(onboarding)" options={{ headerShown: false }} />
@@ -40,7 +36,6 @@ export default function RootLayout() {
           <Stack.Screen name="update-goal" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
-      </ThemeProvider>
     </SafeAreaProvider>
   );
 }
