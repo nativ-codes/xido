a8db59e57e7ffd5a94e5420eeaa84d0445a3724d
Andrei Otea <iAndrew3@protonmail.com>
2025-05-01 22:30:10 +0300

    updated parsing library

--- common/utils/device.ts
+++ common/utils/device.ts
@@ -1,17 +1,25 @@
 import * as DocumentPicker from 'expo-document-picker';
-import { readRemoteFile } from 'react-native-csv';
+import Papa from 'papaparse';
 
 const uploadCsv = async () =>
 	new Promise(async (resolve, reject) => {
 		try {
+			console.log('DocumentPicker.getDocumentAsync');
 			const result = await DocumentPicker.getDocumentAsync({
-				type: 'text/csv'
+				type: ['text/csv', 'application/csv', 'application/vnd.ms-excel', 'text/comma-separated-values']
 			});
 
 			if (result?.assets) {
-				readRemoteFile(result?.assets[0].uri, {
-					complete: resolve
+				const response = await fetch(result?.assets[0].uri);
+				const text = await response.text();
+
+				const jsonTransactions = Papa.parse(text, {
+					header: false,
+					error: function (error, file, inputElem, reason) {
+						reject({ error, file, inputElem, reason });
+					}
 				});
+				resolve(jsonTransactions);
 			} else {
 				console.log('No file selected');
 				reject('No file selected');
